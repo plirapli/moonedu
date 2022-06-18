d631c5da3af93bffdb2900aba0641c312bf310b5
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-06-18 20:32:11 +0700

    fix navbar to position fix

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,11 +4,15 @@ import NavBar from '../components/NavBar';
 function Layout(props) {
   return (
     <>
-      <Outlet />
-      <NavBar
-        activeMenu={props.activeMenu}
-        setActiveMenu={props.setActiveMenu}
-      />
+      <div className='min-h-screen pb-14'>
+        <Outlet />
+      </div>
+      <div className='w-full max-w-screen-sm fixed bottom-0'>
+        <NavBar
+          activeMenu={props.activeMenu}
+          setActiveMenu={props.setActiveMenu}
+        />
+      </div>
     </>
   );
 }
