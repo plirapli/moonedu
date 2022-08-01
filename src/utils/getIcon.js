747f07035623b6604020f8469f525bdfcbb2e752
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-08-01 23:17:41 +0700

    Move get data to separate folders (utils)

---
+++ src/utils/getIcon.js
@@ -0,0 +1,20 @@
+// Img
+import Sport from '../assets/icon/sport.png';
+import Photography from '../assets/icon/photo.png';
+import Paint from '../assets/icon/painting.png';
+import Dashboard from '../assets/icon/dashboard.png';
+import Puzzle from '../assets/icon/puzzle.png';
+import Marketing from '../assets/icon/megaphone.png';
+import Wallet from '../assets/icon/wallet.png';
+import Fishing from '../assets/icon/fishing.png';
+
+export {
+  Sport,
+  Photography,
+  Paint,
+  Dashboard,
+  Puzzle,
+  Marketing,
+  Wallet,
+  Fishing,
+};
