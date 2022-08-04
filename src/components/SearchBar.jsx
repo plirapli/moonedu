604e90209b1d27fb4ef60abee2500c7745a71add
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-08-04 13:23:04 +0700

    remove hover ppinter state on search icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,6 @@
 import { Icon } from '@iconify/react';
 
 const SearchBar = (props) => {
-  const searchHandler = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <>
       <div className='font-medium'>{props.text || 'Search Topic'}</div>
@@ -12,13 +8,9 @@ const SearchBar = (props) => {
         className='mt-2 py-3 px-5 bg-white flex justify-between rounded-xl shadow'
         action=''>
         <div className='flex items-center w-full'>
-          <button
-            onClick={(e) => searchHandler(e)}
-            type='submit'
-            title='Search Courses'
-            className='flex items-center text-gray'>
+          <div className='flex items-center text-gray'>
             <Icon icon='charm:search' width='28' />
-          </button>
+          </div>
           <input
             className='ml-2 outline-none w-full'
             type='text'
