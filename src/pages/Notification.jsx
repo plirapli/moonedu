8fb21995d5644cbad17d231e2d0a7b68b80c5c4a
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-08-06 00:00:00 +0700

    change notification icon

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -3,6 +3,8 @@ import HorIcSubtext from '../components/HorIcSubtext';
 import TopNavBar from '../components/TopNavBar';
 
 const Notification = () => {
+  const icon = 'bi:person-fill';
+
   return (
     <>
       <TopNavBar buttonL={1} buttonR={1} menuName={'Notification'} />
@@ -11,23 +13,23 @@ const Notification = () => {
         <div>
           <div className='font-medium text-gray'>Today</div>
           <div className='mt-2 grid gap-3'>
-            <HorIcSubtext isActive={true} />
-            <HorIcSubtext />
-            <HorIcSubtext />
+            <HorIcSubtext icon={icon} isActive={true} />
+            <HorIcSubtext icon={icon} />
+            <HorIcSubtext icon={icon} />
           </div>
         </div>
         <div className='mt-6'>
           <div className='font-medium text-gray'>Last Week</div>
           <div className='mt-2 grid gap-3'>
-            <HorIcSubtext />
-            <HorIcSubtext />
+            <HorIcSubtext icon={icon} />
+            <HorIcSubtext icon={icon} />
           </div>
         </div>
         <div className='mt-6'>
           <div className='font-medium text-gray'>Last Month</div>
           <div className='mt-2 grid gap-3'>
-            <HorIcSubtext />
-            <HorIcSubtext />
+            <HorIcSubtext icon={icon} />
+            <HorIcSubtext icon={icon} />
           </div>
         </div>
       </div>
